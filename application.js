5c8c2bb20707d9ee583b55450ad551214a6e0aa4
pullingsunflower <tviranam@gmail.com>
2021-09-01 01:34:59 -0700

    commit on test-branch

---
+++ application.js
@@ -0,0 +1,10 @@
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  /* Close/hide the sidenav */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0%";
+  }
+  
